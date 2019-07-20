9d82e747b61fbc55e478f1412694778abc9a4df9
arman <feyziarman@gmail.com>
2019-07-20 22:49:39 +0430

    Add Boilerplate

---
+++ form_login_stateful/lib/src/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+@override
+class App extends StatelessWidget {
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'Form - Login - Stateful',
+      home: Scaffold(
+        body: Text('INIT'),
+      ),
+    );
+  }
+}
