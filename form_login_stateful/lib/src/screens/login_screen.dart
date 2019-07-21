272f7b69ba08a04dbb282d84c1d5ab26acb22b02
arman <feyziarman@gmail.com>
2019-07-21 19:27:50 +0430

    add screen

---
+++ form_login_stateful/lib/src/screens/login_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget{
+  createState(){
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(18.0),
+      child: Form(
+        child: Column(
+          children: [
+            "",
+            ""
+          ],
+        ),
+      ),
+    );
+  }
+}
