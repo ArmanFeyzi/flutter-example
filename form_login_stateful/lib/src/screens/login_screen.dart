69b13212b67354bcc45d96f2d70475ed6aec2f3f
arman <feyziarman@gmail.com>
2019-07-21 20:59:53 +0430

    add form elements Add user and password text field and submit button to login form

--- form_login_stateful/lib/src/screens/login_screen.dart
+++ form_login_stateful/lib/src/screens/login_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatefulWidget{
-  createState(){
+class LoginScreen extends StatefulWidget {
+  createState() {
     return LoginScreenState();
   }
 }
@@ -13,10 +13,51 @@ class LoginScreenState extends State<LoginScreen> {
       child: Form(
         child: Column(
           children: [
-            "",
-            ""
+            emailField(),
+            passwordField(),
+            submitButton(),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: const InputDecoration(
+        icon: Icon(Icons.person),
+        hintText: 'yourmail@gmail.com',
+        labelText: 'Username / Email',
+      ),
+      validator: (String value) {
+        return value.contains('@') ? 'Do not use the @ char.' : null;
+      },
+    );
+  }
+
+  Widget passwordField() {
+    return TextFormField(
+      keyboardType: TextInputType.text,
+      obscureText: true,
+      decoration: const InputDecoration(
+        icon: Icon(Icons.security),
+        hintText: 'your secret password',
+        labelText: 'Password',
+      ),
+      validator: (String value) {
+        return value.contains('1') ? 'Do not use the 1 char.' : null;
+      },
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      onPressed: () {},
+      padding: const EdgeInsets.all(4.0),
+      child: Container(
+        padding: const EdgeInsets.all(7.0),
+        child: const Text('ورود به برنامه', style: TextStyle(fontSize: 18)),
       ),
     );
   }
