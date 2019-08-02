3c777d028c6dd399cf55b3df89669b8804ef83ef
Arman Feyzi <feyziarman@gmail.com>
2019-08-02 23:52:50 +0430

    add Home class

---
+++ animation/lib/src/screens/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class Home extends StatefulWidget {
+  @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  Widget build(BuildContext context) {
+    
+  }
+}
