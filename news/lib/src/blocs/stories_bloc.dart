10884dd22ae825785ff1cd904f3abc395a84569d
Arman Feyzi <feyziarman@gmail.com>
2019-08-14 21:11:31 +0430

    init Stories BLOC

--- news/lib/src/blocs/stories_bloc.dart
+++ news/lib/src/blocs/stories_bloc.dart
@@ -1,3 +1,14 @@
+import 'package:rxdart/rxdart.dart';
+import '../models/item_model.dart';
+import '../resources/repository.dart';
+
 class StoriesBloc {
-  
+  final _topIds = PublishSubject<List<int>>();
+
+  // Getter to Stream
+  Observable<List<int>> get topIds => _topIds.stream;
+
+  dispose() {
+    _topIds.close();
+  }
 }
