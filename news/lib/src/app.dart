34ad008316fc4ffb6ff3faf18ad77236488fce2c
Arman Feyzi <feyziarman@gmail.com>
2019-08-15 14:41:11 +0430

    init RxDart pattern

--- news/lib/src/app.dart
+++ news/lib/src/app.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'screens/news_list.dart';
+import 'blocs/stories_provider.dart';
 
 
 class NewsApp extends StatelessWidget {
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Beh Nazar',
-      home: NewsList(),
+    return StoriesProvider(
+      child: MaterialApp(
+        title: 'Beh Nazar',
+        home: NewsList(),
+      ),
     );
   }
 }
--- news/lib/src/screens/news_list.dart
+++ news/lib/src/screens/news_list.dart
@@ -1,13 +1,40 @@
 import 'package:flutter/material.dart';
-
+import '../blocs/stories_provider.dart';
 
 class NewsList extends StatelessWidget {
   Widget build(BuildContext context) {
+    // By use this BLOC we can render top ids in app
+    // .of() method help us to access availabel SotresProvider and StoreBloc in all entire app
+    // (context) by this 'context' we can access all BLOC hiracy 
+    final bloc = StoriesProvider.of(context);
+
+    bloc.fetchTopIds();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Tejarat News'),
       ),
-      body: Text('body news app data'),
+      body: buildList(bloc),
+    );
+  }
+
+  Widget buildList(StoriesBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.topIds,
+      builder: (context, AsyncSnapshot<List<int>>snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        return ListView.builder(
+          itemCount: snapshot.data.length,
+          itemBuilder: (context, int index) {
+            return Text('${snapshot.data[index]}');
+          },
+        );
+      },
     );
   }
 }
